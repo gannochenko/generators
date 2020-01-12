1a8311de001133a2d10084195f79a0fd183e9d94
Sergei <gannochenko.sv@gmail.com>
2020-01-12 22:15:39 +0000

    style

--- microservice.frontend/template/[application_folder]/src/common/style/global.tsx
+++ microservice.frontend/template/[application_folder]/src/common/style/global.tsx
@@ -1,44 +1,14 @@
-import React, { ComponentType, forwardRef, Ref } from 'react';
 import { createGlobalStyle } from 'styled-components';
-import { fontMaterialIcons } from '@bucket-of-bolts/styled-companion';
-import { ObjectLiteral } from '../../type';
+import { fgColor } from '@bucket-of-bolts/styled-companion';
+import { theme } from './theme';
 
-type Theme = ObjectLiteral;
-type RefUnknown = Ref<unknown>;
-
-export const ThemeContext = React.createContext<Theme>({});
-export const withTheme = (
-    Component: ComponentType<{ ref?: RefUnknown; theme?: Theme }>,
-    path = '',
-) => {
-    const WithTheme = (props: ObjectLiteral, ref: RefUnknown) => (
-        <ThemeContext.Consumer>
-            {value => (
-                <Component
-                    {...props}
-                    ref={ref}
-                    theme={path.length ? value[path] : value}
-                />
-            )}
-        </ThemeContext.Consumer>
-    );
-
-    const wrappedComponentName =
-        Component.displayName || Component.name || 'Component';
-
-    WithTheme.displayName = `withTheme(${wrappedComponentName})`;
-
-    const WithThemeAndRef = forwardRef(WithTheme);
-
-    return WithThemeAndRef;
-};
-
-export const GlobalStyle = withTheme(createGlobalStyle`
-    ${({ theme }: { theme: Theme }) => `
-        ${fontMaterialIcons()}
-        body {
-            color: ${theme.body.color};
-            background-color: ${theme.body.background.color};
-        }
-    `}
-`);
+export const GlobalStyle = createGlobalStyle`
+    body {
+        color: ${theme.body.color};
+        background-color: ${theme.body.background.color};
+    }
+    
+    a {
+      ${fgColor(theme.link.color.hout, theme.link.color.hover, `300ms`)}
+    }
+`;
