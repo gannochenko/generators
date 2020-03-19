5e223dc5f0f93d5b3324a44f7a1f734637e0f545
Sergei <gannochenko.sv@gmail.com>
2020-03-19 17:47:29 +0100

    knobs in story

--- react.component/template/[component_name_pascal]/[?use_stories]__story__/[component_name_pascal].story.tsx
+++ react.component/template/[component_name_pascal]/[?use_stories]__story__/[component_name_pascal].story.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 import { <%- component_name_pascal %> } from '../<%- component_name_pascal %>';
 
 export default {
     title: '<%- component_name_pascal %>',
     component: <%- component_name_pascal %>,
-    decorators: [],
+    decorators: [withKnobs],
     parameters: {},
 };
 
-export const Basic = () => <<%- component_name_pascal %>>Hello, world!</<%- component_name_pascal %>>;
+export const Basic = () => <<%- component_name_pascal %>>{text('Content', 'Hello, world!')}</<%- component_name_pascal %>>;
