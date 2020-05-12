651850ad050112857a20b2c94caec712b7e0c62e
Sergei <gannochenko.sv@gmail.com>
2020-05-12 23:33:58 +0200

    page gen

--- microservice.frontent.page/template/[page_name_camel]/[page_name_pascal].tsx
+++ microservice.frontent.page/template/[page_name_camel]/[page_name_pascal].tsx
@@ -2,33 +2,25 @@ import React, { FunctionComponent } from 'react';
 import { connect } from 'react-redux';
 import { withNotification } from '@bucket-of-bolts/ui';
 import {
-    useErrorNotification,
-    useDispatchUnload,
-    withClient,
-    useDispatchLoad,
+    usePage,
 } from '../../lib';
+import { Container, Layout, Link, SEO } from '../../components';
 
-import { Layout } from '../../components';
 import { <%- page_name_pascal %>PageProperties } from './type';
 import { mapDispatchToProps } from './dispatch';
 
 import { ObjectLiteral } from '../../../type';
 
-const <%- page_name_pascal %>PageComponent: FunctionComponent<<%- page_name_pascal %>PageProperties> = ({
-    dispatchLoad,
-    dispatchUnload,
-    client,
-    error,
-    notify,
-}) => {
-    useDispatchLoad(dispatchLoad, client);
-    useDispatchUnload(dispatchUnload);
-    useErrorNotification(error, notify);
+const <%- page_name_pascal %>PageComponent: FunctionComponent<<%- page_name_pascal %>PageProperties> = (props) => {
+    usePage(props);
 
     return (
-        <Layout title="This is the title of the page">
-            This is the content of the page
-        </Layout>
+        <>
+            <SEO title="<%- page_name %>" />
+            <Container>
+                <Link to="/">Home</Link>
+            </Container>
+        </>
     );
 };
 
@@ -39,4 +31,10 @@ export const <%- page_name_pascal %>Page = withNotification(
             mapDispatchToProps,
         )(<%- page_name_pascal %>PageComponent),
     ),
+);
+
+export const <%- page_name_pascal %>PageRenderer: RendererType = () => (
+    <Layout>
+        <<%- page_name_pascal %>Page />
+    </Layout>
 );
