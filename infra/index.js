891a93498122f80dbc11daff67b9119b7918ab1f
Sergei <gannochenko.sv@gmail.com>
2021-06-04 15:05:00 +0200

    infra

--- infra/index.js
+++ infra/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports.Generator = class Generator {
     getName() {
         // this is the name your generator will appear in the list under
-        return 'Infra';
+        return 'Project infrastructure';
     }
 
     async getQuestions() {
