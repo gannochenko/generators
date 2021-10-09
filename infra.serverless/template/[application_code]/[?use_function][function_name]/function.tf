d260f08b3f1e9d8410c95afdeef738dece402b71
Sergei <gannochenko.sv@gmail.com>
2021-10-09 23:39:53 +0200

    wip

--- infra.serverless/template/[application_code]/[?use_function][function_name]/function.tf
+++ infra.serverless/template/[application_code]/[?use_function][function_name]/function.tf
@@ -2,6 +2,7 @@ resource "aws_lambda_function" "<%- function_name %>" {
   function_name = local.lambda_function_name
 
   filename = "function-dummy-src.zip"
+  source_code_hash = filebase64sha256("function-dummy-src.zip")
 
   handler = "main.handler"
   runtime = "nodejs14.x"
